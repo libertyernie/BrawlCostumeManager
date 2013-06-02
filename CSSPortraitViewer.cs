3fd7ce1ae4ba7d5b444026cc46f91437fc2adc8d
libertyernie <libertyernie0@gmail.com>
2013-06-01 21:49:46 -0500

    Allow use of sc_selcharacter.pac instead of common5.pac

--- CSSPortraitViewer.cs
+++ CSSPortraitViewer.cs
@@ -12,61 +12,64 @@ using System.IO;
 
 namespace BrawlCharacterManager {
 	public partial class CSSPortraitViewer : UserControl {
+		/// <summary>
+		/// The common5 currently being used. If using sc_selcharacter.pac instead, this will be null.
+		/// </summary>
 		private ResourceNode common5;
-		/*private ResourceNode[] char_bust_tex_nodes;*/
-		private BRESNode from_common5_brres;
-		private TEX0Node from_common5_tex0;
-		/*private TEX0Node from_char_bust_tex;*/
+		/// <summary>
+		/// Either the sc_selcharacter_en archive within common5.pac or the sc_selcharacter.pac file.
+		/// </summary>
+		private ResourceNode sc_selcharacter;
+		private BRESNode brres;
+		private TEX0Node tex0;
 
 		public CSSPortraitViewer() {
 			InitializeComponent();
-			/*char_bust_tex_nodes = new ResourceNode[47];
-			for (int i = 0; i < 47; i++) {
-				if (Constants.CharactersByCSSOrder[i] != null) {
-					char_bust_tex_nodes[i] = NodeFactory.FromFile(null,
-						"menu/common/char_bust_tex/MenSelchrFaceB"
-						+ (i * 10).ToString("D3") + ".brres");
-				}
-			}*/
 			try {
-				common5 = NodeFactory.FromFile(null, "system/common5_en.pac");
+				common5 = null;
+				sc_selcharacter = NodeFactory.FromFile(null, "menu2/sc_selcharacter.pac");
 				UpdateImage(1, 0);
-			} catch (DirectoryNotFoundException) {
-
+			} catch (IOException) {
+				try {
+					common5 = NodeFactory.FromFile(null, "system/common5_en.pac");
+					sc_selcharacter = common5.FindChild("sc_selcharacter_en", false);
+					UpdateImage(1, 0);
+				} catch (IOException) {
+					label1.Text = "Could not load sc_selcharacter or common5.";
+				}
 			}
+			
 		}
 
 		public void UpdateImage(int charNum, int costumeNum) {
 			string tex_number = (charNum*10 + costumeNum + 1).ToString("D3");
 
-			from_common5_tex0 = null;
-			from_common5_brres = null;
+			tex0 = null;
+			brres = null;
 			panel1.BackgroundImage = null;
-			string str1 = "sc_selcharacter_en/char_bust_tex_lz77/MiscData[" + charNum + "]";
+
+			if (common5 != null) {
+				label1.Text = "common5.pac/sc_selcharacter_en/";
+			} else if (sc_selcharacter != null) {
+				label1.Text = "sc_selcharacter.pac/";
+			}
+
+			string str1 = "char_bust_tex_lz77/MiscData[" + charNum + "]";
 			string str2 = "Textures(NW4R)/MenSelchrFaceB." + tex_number;
-			label1.Text = str1 + "//" + str2;
-			ResourceNode from_common5_brres_node = common5.FindChild(str1, false);
+			label1.Text += str1 + "/" + str2;
+			ResourceNode from_common5_brres_node = sc_selcharacter.FindChild(str1, false);
 			if (from_common5_brres_node is BRESNode) {
-				from_common5_brres = (BRESNode)from_common5_brres_node;
-				ResourceNode from_common5_node = from_common5_brres.FindChild(str2, false);
+				brres = (BRESNode)from_common5_brres_node;
+				ResourceNode from_common5_node = brres.FindChild(str2, false);
 				if (from_common5_node is TEX0Node) {
-					from_common5_tex0 = (TEX0Node)from_common5_node;
-					Bitmap bitmap = from_common5_tex0.GetImage(0);
+					tex0 = (TEX0Node)from_common5_node;
+					Bitmap bitmap = tex0.GetImage(0);
 					panel1.BackgroundImage = bitmap;
+				} else {
+					label1.Text += " (tex0 not found)";
 				}
 			}
 
-/*			from_char_bust_tex = null;
-			ResourceNode from_char_bust_tex_node =
-				char_bust_tex_nodes[charNum].FindChild("Textures(NW4R)/MenSelchrFaceB." + tex_number, false);
-			label1.Text = char_bust_tex_nodes[charNum] + "/Textures(NW4R)/MenSelchrFaceB." + tex_number;
-			if (from_char_bust_tex_node is TEX0Node) {
-				from_char_bust_tex = (TEX0Node)from_char_bust_tex_node;
-				Bitmap bitmap = from_char_bust_tex.GetImage(0);
-				panel1.BackgroundImage = bitmap;
-			}*/
-
-//			Invalidate();
 		}
 	}
 }
