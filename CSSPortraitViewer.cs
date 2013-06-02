42d495f786631a54dfc9a5c3fab98da635eadc56
libertyernie <libertyernie0@gmail.com>
2013-06-01 21:28:14 -0500

    Keep the brres that contains all a char's portraits in a variable; the idea is that this can be exported to the proper file in char_bust_tex.
    Commented-out lines will probably be removed in the next revision.

--- CSSPortraitViewer.cs
+++ CSSPortraitViewer.cs
@@ -13,10 +13,21 @@ using System.IO;
 namespace BrawlCharacterManager {
 	public partial class CSSPortraitViewer : UserControl {
 		private ResourceNode common5;
-		private TEX0Node tex0;
+		/*private ResourceNode[] char_bust_tex_nodes;*/
+		private BRESNode from_common5_brres;
+		private TEX0Node from_common5_tex0;
+		/*private TEX0Node from_char_bust_tex;*/
 
 		public CSSPortraitViewer() {
 			InitializeComponent();
+			/*char_bust_tex_nodes = new ResourceNode[47];
+			for (int i = 0; i < 47; i++) {
+				if (Constants.CharactersByCSSOrder[i] != null) {
+					char_bust_tex_nodes[i] = NodeFactory.FromFile(null,
+						"menu/common/char_bust_tex/MenSelchrFaceB"
+						+ (i * 10).ToString("D3") + ".brres");
+				}
+			}*/
 			try {
 				common5 = NodeFactory.FromFile(null, "system/common5_en.pac");
 				UpdateImage(1, 0);
@@ -26,19 +37,35 @@ namespace BrawlCharacterManager {
 		}
 
 		public void UpdateImage(int charNum, int costumeNum) {
-			tex0 = null;
+			string tex_number = (charNum*10 + costumeNum + 1).ToString("D3");
+
+			from_common5_tex0 = null;
+			from_common5_brres = null;
 			panel1.BackgroundImage = null;
-			string str = "sc_selcharacter_en/char_bust_tex_lz77/MiscData["
-			+ charNum
-			+ "]/Textures(NW4R)/MenSelchrFaceB."
-			+ (charNum*10 + costumeNum + 1).ToString("D3");
-			label1.Text = str;
-			ResourceNode MenSelchrFaceB_001 = common5.FindChild(str, true);
-			if (MenSelchrFaceB_001 is TEX0Node) {
-				tex0 = (TEX0Node)MenSelchrFaceB_001;
-				Bitmap bitmap = tex0.GetImage(0);
-				panel1.BackgroundImage = bitmap;
+			string str1 = "sc_selcharacter_en/char_bust_tex_lz77/MiscData[" + charNum + "]";
+			string str2 = "Textures(NW4R)/MenSelchrFaceB." + tex_number;
+			label1.Text = str1 + "//" + str2;
+			ResourceNode from_common5_brres_node = common5.FindChild(str1, false);
+			if (from_common5_brres_node is BRESNode) {
+				from_common5_brres = (BRESNode)from_common5_brres_node;
+				ResourceNode from_common5_node = from_common5_brres.FindChild(str2, false);
+				if (from_common5_node is TEX0Node) {
+					from_common5_tex0 = (TEX0Node)from_common5_node;
+					Bitmap bitmap = from_common5_tex0.GetImage(0);
+					panel1.BackgroundImage = bitmap;
+				}
 			}
+
+/*			from_char_bust_tex = null;
+			ResourceNode from_char_bust_tex_node =
+				char_bust_tex_nodes[charNum].FindChild("Textures(NW4R)/MenSelchrFaceB." + tex_number, false);
+			label1.Text = char_bust_tex_nodes[charNum] + "/Textures(NW4R)/MenSelchrFaceB." + tex_number;
+			if (from_char_bust_tex_node is TEX0Node) {
+				from_char_bust_tex = (TEX0Node)from_char_bust_tex_node;
+				Bitmap bitmap = from_char_bust_tex.GetImage(0);
+				panel1.BackgroundImage = bitmap;
+			}*/
+
 //			Invalidate();
 		}
 	}
