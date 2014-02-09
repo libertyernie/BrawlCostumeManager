3404f26127684436fb1002ba907f90cc3d41215e
libertyernie <libertyernie0@gmail.com>
2014-02-09 10:42:35 -0600

    New build that requires BrawlManagerLib.dll, but tells you if it can't find it

--- Program.cs
+++ Program.cs
@@ -13,12 +13,23 @@ namespace BrawlCostumeManager {
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 
+			try {
+				trylib();
+			} catch (Exception e) {
+				MessageBox.Show(null, "Could not load BrawlManagerLib.dll.", e.GetType().ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
 			var D = BrawlLib.Properties.Settings.Default;
 			if (D.GetType().GetProperty("HideMDL0Errors") != null) {
 				D.GetType().InvokeMember("HideMDL0Errors", System.Reflection.BindingFlags.SetProperty, null, D, new object[] { true });
 			}
 
 			Application.Run(new CostumeManager());
+		}
+
+		static void trylib() {
+			var Q = new BrawlManagerLib.CollapsibleSplitter();
 		}
 	}
 }
